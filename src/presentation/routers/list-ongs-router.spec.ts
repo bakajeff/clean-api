27084650c49178af97911140d4b37cad69f51371
@bakajeff <jefftewf17@outlook.com>
2022-07-04 16:28:07 -0300

    refactor: change test subject to sut

--- src/presentation/routers/list-ongs-router.spec.ts
+++ src/presentation/routers/list-ongs-router.spec.ts
@@ -24,29 +24,29 @@ function ListOngsRouter (listOngsUseCase: ListOngsUseCaseType) {
 describe('ListOngsRouter', () => {
   it('calls ListOngsUseCase once', async () => {
     const listOngsUseCase = ListOngsUseCase()
-    const listOngsRouter = ListOngsRouter(listOngsUseCase)
+    const sut = ListOngsRouter(listOngsUseCase)
     const listOngsUseCaseSpy = jest.spyOn(listOngsUseCase, 'perform')
 
-    await listOngsRouter.perform()
+    await sut.perform()
 
     expect(listOngsUseCaseSpy).toHaveBeenCalledTimes(1)
   })
   it('retuns 500 if ListOngsUseCase throws', async () => {
     const listOngsUseCase = ListOngsUseCase()
-    const listOngsRouter = ListOngsRouter(listOngsUseCase)
+    const sut = ListOngsRouter(listOngsUseCase)
 
     jest.spyOn(listOngsUseCase, 'perform').mockImplementationOnce(() => {
       throw new Error()
     })
-    const httpResponse = await listOngsRouter.perform()
+    const httpResponse = await sut.perform()
 
     expect(httpResponse.statusCode).toBe(500)
   })
   it('returns 200 on success', async () => {
     const listOngsUseCase = ListOngsUseCase()
-    const listOngsRouter = ListOngsRouter(listOngsUseCase)
+    const sut = ListOngsRouter(listOngsUseCase)
 
-    const httpResponse = await listOngsRouter.perform()
+    const httpResponse = await sut.perform()
 
     expect(httpResponse.statusCode).toBe(200)
   })
