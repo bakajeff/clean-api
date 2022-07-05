f77694be5f9abe689e436281372fa117841ab129
@bakajeff <jefftewf17@outlook.com>
2022-07-05 10:23:29 -0300

    feat: create incident model

---
+++ src/domain/models/incident.ts
@@ -0,0 +1,14 @@
+export type IncidentType = {
+  title: string,
+  description: string,
+  value: number,
+  ongId: string
+}
+
+export type IncidentModel = {
+  id: string,
+  title: string,
+  description: string,
+  value: number,
+  ongId: string
+}
