23b0b1c5837e6cfc80c17803353152d03bb2765e
@bakajeff <jefftewf17@outlook.com>
2022-07-05 09:48:56 -0300

    test: calls CreateIncidentRepository with correct values

--- src/domain/usecases/create-incidente-usecase.spec.ts
+++ src/domain/usecases/create-incidente-usecase.spec.ts
@@ -1,9 +1,35 @@
+import crypto from 'crypto'
+
+type IncidentType = {
+  title: string,
+  description: string,
+  value: number,
+  ongId: string
+}
+
 function CreateIncidentUseCase (createIncidentRepository: any) {
-  async function perform () {
-    createIncidentRepository.perform()
+  async function perform (incident: IncidentType) {
+    createIncidentRepository.perform(incident)
   }
   return {
     perform
+  }
+}
+
+function generateRandomText () {
+  return crypto.randomBytes(20).toString('hex')
+}
+
+function generateRandomNumber () {
+  return Math.random() * 2.5
+}
+
+function makeFakeIncidentData () {
+  return {
+    title: generateRandomText(),
+    description: generateRandomText(),
+    value: generateRandomNumber(),
+    ongId: generateRandomText()
   }
 }
 
@@ -15,7 +41,7 @@ describe('CreateIncidentUseCase', () => {
     const sut = CreateIncidentUseCase(createIncidentRepository)
     const createIncidentRepositorySpy = jest.spyOn(createIncidentRepository, 'perform')
 
-    await sut.perform()
+    await sut.perform(makeFakeIncidentData())
 
     expect(createIncidentRepositorySpy).toHaveBeenCalledTimes(1)
   })
@@ -28,8 +54,20 @@ describe('CreateIncidentUseCase', () => {
       throw new Error()
     })
 
-    const promise = sut.perform()
+    const promise = sut.perform(makeFakeIncidentData())
 
     expect(promise).rejects.toThrow()
+  })
+  it('calls CreateIncidentRepository with correct values', async () => {
+    const createIncidentRepository = {
+      perform: jest.fn()
+    }
+    const sut = CreateIncidentUseCase(createIncidentRepository)
+    const fakeIncident = makeFakeIncidentData()
+
+    const createIncidentRepositorySpy = jest.spyOn(createIncidentRepository, 'perform')
+    await sut.perform(fakeIncident)
+
+    expect(createIncidentRepositorySpy).toHaveBeenCalledWith(fakeIncident)
   })
 })
