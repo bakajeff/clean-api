79dcb9f713ed383cc0923328232f64f1ba49b562
@bakajeff <jefftewf17@outlook.com>
2022-07-05 13:38:26 -0300

    test: returns 400 if no description

--- src/presentation/routers/create-incident-router.spec.ts
+++ src/presentation/routers/create-incident-router.spec.ts
@@ -1,9 +1,13 @@
 import { CreateIncidentRepositoryType } from '../../domain/repositories/create-incident-repository'
-import { badRequest, HttpRequestType, HttpResponseType } from '../helpers/http-helper'
+import { MissingParamError } from '../../utils/errors/missing-param-error'
+import { badRequest, HttpRequestType, HttpResponseType, ok } from '../helpers/http-helper'
 
 function CreateIncidentRouter (createIncidentRepository: CreateIncidentRepositoryType) {
   async function perform (httpRequest: HttpRequestType): Promise<HttpResponseType> {
-    return badRequest(new Error('title is a required field'))
+    if (!httpRequest.body.title) return badRequest(new MissingParamError('title'))
+    if (!httpRequest.body.description) return badRequest(new MissingParamError('description'))
+
+    return ok('ok')
   }
   return {
     perform
@@ -26,6 +30,21 @@ describe('CreateIncidentRouter', () => {
 
     const httpResponse = await sut.perform(httpRequest)
 
-    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('title')))
+  })
+  it('returns 400 if no description', async () => {
+    const sut = CreateIncidentRouter(createIncidentRepository)
+    const httpRequest = {
+      body: {
+        title: 'any title',
+        // description: 'any description',
+        value: 245453.53453,
+        ongId: 'any ong id'
+      }
+    }
+
+    const httpResponse = await sut.perform(httpRequest)
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('description')))
   })
 })
